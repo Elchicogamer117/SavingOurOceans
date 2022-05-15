b65b898fb5224917a73666d75cc67987d12e264a
AliciaGaona <aligaovela@gmail.com>
2022-05-15 00:06:18 -0500

    Se agrega controller Base para preparar recibir API

---
+++ SavingOurOceansFrontEnd/SavingOurOceansFrontEnd/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SavingOurOceansFrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SavingOurOceansFrontEnd.Controllers
+{
+    public class BaseController : Controller
+    {
+       
+
+      
+    }
+}
