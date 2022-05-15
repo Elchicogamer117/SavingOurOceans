ad2b1d039c437f999ee04edffdb2277e8a105820
AliciaGaona <aligaovela@gmail.com>
2022-05-15 15:51:24 -0500

    Se corrige referencia de controller test y se comenta prueba de llamado api, para similar respuesta con datos json

--- test/controllers/ControllerConsimeApi.test.js
+++ test/controllers/ControllerConsimeApi.test.js
@@ -1,11 +1,3 @@
-const ControllerConsimeApi = require('/../../../../../Users/Novucentral/savingouroceansfrtnd/app/lib/controllers/ControllerConsimeApi')
+const ControllerConsimeApi = require('./../../lib/services/ConsumeServicioApiTaskS')
 
 
-describe("Test service ConsumeServicioApiRewards", ()=>{
-    test("1.Probando metodo static getExplorersByMission, que recibe lista de explorers", ()=>{
-       
-        expect(getExplorers).toBeDefined()
-    })
-
-   
-})
