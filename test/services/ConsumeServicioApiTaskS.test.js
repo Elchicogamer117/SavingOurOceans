4f6e8e6639749f3ecc350cd6b57d6bcd719ec7b7
AliciaGaona <aligaovela@gmail.com>
2022-05-15 16:18:11 -0500

    Corregimos tipo de llamado de la descripcion en prueba unitaria  para createTask de ApiTask

--- test/services/ConsumeServicioApiTaskS.test.js
+++ test/services/ConsumeServicioApiTaskS.test.js
@@ -3,14 +3,13 @@ const Reader= require('./../../lib/utils/Reader')
 const task = Reader.readJsonFile("taskDummyVistas.json"); 
 
 
-test("1.Consumir servicio get Info de Api task", ()=>{
+test("1.Consumir servicio post Info de Api task", ()=>{
     const taskId = task.id;
     const respuestagetInfo = ConsumeServicioApiTaskS.getInfo(taskId);
     expect(respuestagetInfo).toBeDefined();
 })
 
-test("1.Consumir servicio get Info de Api createTask", ()=>{
-    
+test("2.Consumir servicio post Info de Api createTask", ()=>{ 
     const respuestagetInfo = ConsumeServicioApiTaskS.createTask(task);
     expect(task).toBeDefined();
 })
